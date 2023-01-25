71c6ae2be28f775e278ea7a878dcc7f132fb19af
Doxakis1 <mike.karat@yahoo.com>
2023-01-25 16:56:40 +0200

    Somewhat works

--- Push_Swap_From_Scratch/main.c
+++ Push_Swap_From_Scratch/main.c
@@ -6,7 +6,7 @@
 /*   By: mkaratzi <mkaratzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 16:19:00 by mkaratzi          #+#    #+#             */
-/*   Updated: 2023/01/24 20:40:07 by mkaratzi         ###   ########.fr       */
+/*   Updated: 2023/01/25 16:56:24 by mkaratzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,100 +37,116 @@ int	main(int ac, char const **av)
 int check_more_rotates(t_stack **stack_given, int bitshift, int check, int stack_size)
 {
 	int j;
-
+	t_stack current;
+	current = *stack_given[0];
 	j = 0;
 	while( j < stack_size)
 			{
-				if((stack_given[0]->nbr / bitshift % 2) == check)
+				if((current.nbr / bitshift % 2) == check)
 				{
 					return (1);
 				}
-				stack_given[0] = stack_given[0]->next;
+				current = *current.next;
 				j++;
 			}
 	return (0);
 }
-int sort_the_stack(t_stack *stack_a, int stack_size)
+int get_bitshift(int stack_size)
+{
+	int i;
+	int answer;
+
+	answer = 1;
+	i = 1;
+	while(stack_size / i)
+	{
+		i *= 2;
+		answer++;
+	}
+		
+	return (answer);
+
+}
+int sort_the_stack(t_stack *stack_a, int a_stack_size)
 {
 		t_stack	*stack_b;
 		int		bitshift;
+		int		unsorted = a_stack_size;
 		int		b_stack_size = 0;
-		int		stack_size_holder = stack_size;
+		int		stack_size_holder = a_stack_size;
 		int		i = 0;
 		int		j = 0;
 		int		pushed_happened = 0;
-		int		holder = 1;	
+		int		holder = get_bitshift(stack_size_holder);	
 		stack_b = NULL;
 		i = 0;
-		bitshift = 1;
-
-	while( holder <= 64)
+		bitshift = 8;
+	while(bitshift)
 	{
 		bitshift = holder;
 		while(stack_a)
 		{
 			j = 0;
-			while( j < stack_size)
+			if (!check_more_rotates(&stack_a, bitshift, 0, a_stack_size))
+				bitshift *= 2;
+			while(stack_a && j < a_stack_size && check_more_rotates(&stack_a, bitshift, 0, a_stack_size))
 			{
 				if((stack_a->nbr / bitshift % 2) == 0)
 				{
 					push_to_stack(&stack_a, &stack_b, 'b');
 					b_stack_size++;
+					if (stack_b->nbr == unsorted)
+					{
+						stack_b->nbr += 2147483648;
+						reverse_rotate_move(&stack_b, 'b');
+						unsorted--;
+						stack_size_holder--;
+						b_stack_size--;
+					}	
 				}
 				else
 					reverse_rotate_move(&stack_a, 'a');
 				j++;	
 			}
-				stack_size = stack_size_holder - b_stack_size;
+				a_stack_size = stack_size_holder - b_stack_size;
 				bitshift *= 2;
 		}
-		bitshift = holder;
-		while(stack_b)
+		while(stack_b && stack_b->nbr < 2147483648)
 		{
-			j = 0;
-			pushed_happened = 0;
-			while( j < b_stack_size)
+			if (stack_b->nbr == unsorted)
 			{
-				if((stack_b->nbr / bitshift % 2) == 1)
-				{
-					push_to_stack(&stack_b, &stack_a, 'a');
-					stack_size++;
-					pushed_happened = 1;
-				}
-				else
-					reverse_rotate_move(&stack_b, 'b');
-				j++;	
+				stack_b->nbr += 2147483648;
+				reverse_rotate_move(&stack_b, 'b');
+				unsorted--;
+				stack_size_holder--;
+				b_stack_size--;
+				
+			}	
+			else
+			{
+				push_to_stack(&stack_b, &stack_a, 'a');
+				a_stack_size++;
 			}
-			if (!pushed_happened)
-				break ;
-			b_stack_size = stack_size_holder - stack_size;
-			bitshift *= 2;
+			b_stack_size = stack_size_holder - a_stack_size;
 		}
-		holder *= 2;
-		i++;
+		holder--;
 	}
 	while(stack_b)
-		{
-			push_to_stack(&stack_b, &stack_a, 'a');
-		}
+	{
+		stack_b->nbr -= 2147483648;
+		push_to_stack(&stack_b, &stack_a, 'a');
+		a_stack_size++;
+	}
+
+		
+		
 		// ft_printf("Our A+stack size is: %d\nOur B+stack size is: %d\n", stack_size, b_stack_size);
 		// ft_printf("Our bstack size is: %d\n", stack_size);
-		// while (++i <= stack_size_holder && stack_a)
-		// {
-		// 	ft_printf("A stack: %d\n", stack_a->nbr);
-		// 	stack_a = stack_a->next;
-		// }
 		i = 0;
-		// while (++i <= stack_size_holder && stack_a)
+		// while (++i <= a_stack_size && stack_a)
 		// {
 		// 	ft_printf("A stack: %d\n", stack_a->nbr);
 		// 	stack_a = stack_a->next;
-		// }
-		// i = 0;
-		// while (++i <= stack_size_holder && stack_b)
-		// {
-		// 	ft_printf("B stack: %d\n", stack_b->nbr);
-		// 	stack_b = stack_b->next;
 		// }
 		return (1);
 }
