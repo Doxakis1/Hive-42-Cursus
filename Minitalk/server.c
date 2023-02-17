f63daa0e46714565fffac370a3f19253d5e21e95
Doxakis1 <mike.karat@yahoo.com>
2023-02-17 17:22:38 +0200

    Done some Minitalk stuff

--- Minitalk/server.c
+++ Minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: mkaratzi <mkaratzi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 16:39:19 by mkaratzi          #+#    #+#             */
-/*   Updated: 2023/02/17 16:50:21 by mkaratzi         ###   ########.fr       */
+/*   Updated: 2023/02/17 17:21:07 by mkaratzi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,24 @@
 #include <stdio.h>
 #include <signal.h>
 
+void handle_signals(int signal, siginfo_t *singal_info)
+{
+	printf("we received a signal from: %d\n", singal_info->si_pid);
+}
+
 int main(void)
 {
 	struct sigaction my_handler;
 
-	my_handler.sa_sigaction = (void *)0;
+	my_handler.__sigaction_u.__sa_sigaction = (void *)0;
+	my_handler.__sigaction_u.__sa_handler = (void *)handle_signals;
+	my_handler.sa_flags = SA_SIGINFO;
 	sigemptyset(&(my_handler.sa_mask));
-	printf("Server is now running and our pid is: %d\n", getpid());
+	sigaction(SIGTSTP, &my_handler, NULL);
+	printf("Server is now running and our pid is: %d and our parent pid is: %d\n", getpid(), getppid());
+	while(1)
+	{
+		
+	}
 	return 0;
 }
