1ab844027a849da77123baba74b51e45cd77b79a
Doxakis1 <mike.karat@yahoo.com>
2023-12-19 19:15:33 +0200

    added error cheking

--- CPP_Modules/CPP04/ex01/Cat.cpp
+++ CPP_Modules/CPP04/ex01/Cat.cpp
@@ -6,20 +6,16 @@ Cat::Cat() : Animal("Cat")
   this->brain = new Brain();
 }
 
-Cat::Cat(const Cat &copy) : Animal(copy)
+Cat::Cat(const Cat &copy) : Animal(copy) , brain(new Brain(*copy.brain))
 {
-   std::cout << "Cat copy constructor called" << std::endl;
-   this->brain = NULL;
-   *this = copy; 
+   std::cout << "Cat copy constructor called" << std::endl; 
 }
 
 Cat &Cat::operator=(const Cat &other)
 {
   std::cout << "Cat copy assignment operator called" << this->brain << std::endl;
   this->m_type = other.m_type; //well any cat should be already same
-  if (this->brain)
-    delete this->brain;
-  this->brain = new Brain(*other.brain); // this is done because we want a copy of the other brain but not just a copy of the pointer 
+  *this->brain = *other.brain;
   return (*this);
 }
 
--- CPP_Modules/CPP04/ex02/Dog.cpp
+++ CPP_Modules/CPP04/ex02/Dog.cpp
@@ -6,20 +6,16 @@ Dog::Dog() : Animal("Dog")
   this->brain = new Brain();
 }
 
-Dog::Dog(const Dog &copy) : Animal(copy)
+Dog::Dog(const Dog &copy) : Animal(copy) , brain(new Brain(*copy.brain))
 {
-   std::cout << "Dog copy constructor called" << std::endl;
-   this->brain = NULL;
-   *this = copy; 
+   std::cout << "Dog copy constructor called" << std::endl; 
 }
 
 Dog &Dog::operator=(const Dog &other)
 {
   std::cout << "Dog copy assignment operator called" << this->brain << std::endl;
   this->m_type = other.m_type; //well any Dog should be already same
-  if (this->brain)
-    delete this->brain;
-  this->brain = new Brain(*other.brain); // this is done because we want a copy of the other brain but not just a copy of the pointer 
+  *this->brain = *other.brain;
   return (*this);
 }
 
