58d8fda74b7cf2162b8b95ac87862e82cde1376c
Doxakis1 <mike.karat@yahoo.com>
2023-12-19 09:36:46 +0200

    fixed minor bugs

--- CPP_Modules/CPP04/ex01/Dog.cpp
+++ CPP_Modules/CPP04/ex01/Dog.cpp
@@ -8,17 +8,18 @@ Dog::Dog() : Animal("Dog")
 
 Dog::Dog(const Dog &copy) : Animal(copy.m_type)
 {
-   *this = copy; 
    std::cout << "Dog copy constructor called" << std::endl;
-   this->brain = new Brain(*copy.brain); // this is done because we otherwise have a copy of the pointer to the copy brain
+   this->brain = NULL;
+   *this = copy; 
 }
 
 Dog &Dog::operator=(const Dog &other)
 {
-  this->m_type = other.m_type; //well any Dog should be already same 
-  delete this->brain;
-  this->brain = new Brain(*other.brain); // this is done because we want a copy of the other brain but not just a copy of the pointer
-  std::cout << "Dog copy assignment operator called" << std::endl;
+  std::cout << "Dog copy assignment operator called" << this->brain << std::endl;
+  this->m_type = other.m_type; //well any Dog should be already same
+  if (this->brain)
+    delete this->brain;
+  this->brain = new Brain(*other.brain); // this is done because we want a copy of the other brain but not just a copy of the pointer 
   return (*this);
 }
 
