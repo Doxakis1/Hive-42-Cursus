d8faf95987128011b5d7351cc6df2e1a072c17f3
Doxakis1 <mike.karat@yahoo.com>
2023-12-19 13:21:17 +0200

    Donzo

--- CPP_Modules/CPP02/ex02/Fixed.hpp
+++ CPP_Modules/CPP02/ex02/Fixed.hpp
@@ -39,10 +39,10 @@ class Fixed {
         float   toFloat() const;
         int     toInt() const;
 
-        static const Fixed &min(const Fixed &lhs, const Fixed& rhs);
-        static const Fixed &max(const Fixed &lhs, const Fixed& rhs);
-        static Fixed &min(Fixed &lhs, Fixed& rhs);
-        static Fixed &max(Fixed &lhs, Fixed& rhs);
+        static const Fixed &min(const Fixed &first, const Fixed& second);
+        static const Fixed &max(const Fixed &first, const Fixed& second);
+        static Fixed &min(Fixed &first, Fixed& second);
+        static Fixed &max(Fixed &first, Fixed& second);
 };
 
 std::ostream&   operator<<(std::ostream& os, const Fixed &fixed);
